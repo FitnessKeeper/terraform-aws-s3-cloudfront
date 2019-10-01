af598a6ae6206e5959260c91c729894e9e7034ca
Anuj Modi <anuj.modi@asics.com>
2019-10-01 14:46:26 -0400

    feat: Update to terraform 0.12.9

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 # Data tier - S3
 
 resource "aws_cloudfront_origin_access_identity" "identity" {
-  count   = "${(var.cloudfront_origin_access_identity_path == "") ? 1 : 0}"
+  count   = var.cloudfront_origin_access_identity_path == "" ? 1 : 0
   comment = "CloudFront access to S3 bucket ${var.bucket_name}"
 }
 
@@ -12,8 +12,14 @@ data "aws_iam_policy_document" "bucket_policy_document" {
     resources = ["arn:aws:s3:::${var.bucket_name}${var.iam_policy_resources_path}"]
 
     principals {
-      type        = "AWS"
-      identifiers = ["${(var.cloudfront_origin_access_identity_path == "") ? element(concat(aws_cloudfront_origin_access_identity.identity.*.iam_arn, list("")), 0) : var.cloudfront_origin_access_identity_iam_arn}"]
+      type = "AWS"
+      identifiers = [var.cloudfront_origin_access_identity_path == "" ? element(
+        concat(
+          aws_cloudfront_origin_access_identity.identity.*.iam_arn,
+          [""],
+        ),
+        0,
+      ) : var.cloudfront_origin_access_identity_iam_arn]
     }
   }
 
@@ -23,36 +29,63 @@ data "aws_iam_policy_document" "bucket_policy_document" {
     resources = ["arn:aws:s3:::${var.bucket_name}"]
 
     principals {
-      type        = "AWS"
-      identifiers = ["${(var.cloudfront_origin_access_identity_path == "") ? element(concat(aws_cloudfront_origin_access_identity.identity.*.iam_arn, list("")), 0) : var.cloudfront_origin_access_identity_iam_arn}"]
+      type = "AWS"
+      identifiers = [var.cloudfront_origin_access_identity_path == "" ? element(
+        concat(
+          aws_cloudfront_origin_access_identity.identity.*.iam_arn,
+          [""],
+        ),
+        0,
+      ) : var.cloudfront_origin_access_identity_iam_arn]
     }
   }
 }
 
 resource "aws_s3_bucket" "bucket" {
-  count         = "${var.create_bucket ? 1 : 0}"
-  provider      = "aws.s3"
-  bucket        = "${var.bucket_name}"
-  acl           = "${var.bucket_acl}"
-  region        = "${data.aws_region.s3_region.name}"
-  force_destroy = "${var.bucket_force_destroy}"
-  policy        = "${data.aws_iam_policy_document.bucket_policy_document.json}"
+  count         = var.create_bucket ? 1 : 0
+  provider      = aws.s3
+  bucket        = var.bucket_name
+  acl           = var.bucket_acl
+  region        = data.aws_region.s3_region.name
+  force_destroy = var.bucket_force_destroy
+  policy        = data.aws_iam_policy_document.bucket_policy_document.json
 
   cors_rule {
-    allowed_headers = "${var.bucket_cors_allowed_headers}"
-    allowed_methods = "${var.bucket_cors_allowed_methods}"
-    allowed_origins = ["${compact(distinct(concat(list(var.cloudfront_fqdn), var.cloudfront_aliases, var.bucket_cors_extra_allowed_origins)))}"]
-    expose_headers  = "${var.bucket_cors_expose_headers}"
-    max_age_seconds = "${var.bucket_cors_max_age_seconds}"
+    allowed_headers = var.bucket_cors_allowed_headers
+    allowed_methods = var.bucket_cors_allowed_methods
+    allowed_origins = compact(
+      distinct(
+        concat(
+          [var.cloudfront_fqdn],
+          var.cloudfront_aliases,
+          var.bucket_cors_extra_allowed_origins,
+        ),
+      ),
+    )
+    expose_headers  = var.bucket_cors_expose_headers
+    max_age_seconds = var.bucket_cors_max_age_seconds
   }
 }
 
 # outputs from data tier
 
 output "cloudfront_origin_access_identity_iam_arn" {
-  value = "${element(concat(aws_cloudfront_origin_access_identity.identity.*.iam_arn, list("")), 0)}"
+  value = element(
+    concat(
+      aws_cloudfront_origin_access_identity.identity.*.iam_arn,
+      [""],
+    ),
+    0,
+  )
 }
 
 output "cloudfront_origin_access_identity_path" {
-  value = "${element(concat(aws_cloudfront_origin_access_identity.identity.*.cloudfront_access_identity_path, list("")), 0)}"
+  value = element(
+    concat(
+      aws_cloudfront_origin_access_identity.identity.*.cloudfront_access_identity_path,
+      [""],
+    ),
+    0,
+  )
 }
+
--- edge.tf
+++ edge.tf
@@ -1,104 +1,111 @@
 # Edge - Route 53 and CloudFront
 
 resource "aws_route53_record" "cloudfront_a" {
-  count   = "${(var.cloudfront_fqdn != "") ? 1 : 0}"
-  zone_id = "${data.aws_route53_zone.zone.zone_id}"
-  name    = "${var.cloudfront_fqdn}"
+  count   = var.cloudfront_fqdn != "" ? 1 : 0
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = var.cloudfront_fqdn
   type    = "A"
 
   alias {
-    name                   = "${aws_cloudfront_distribution.cloudfront.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.cloudfront.hosted_zone_id}"
+    name                   = aws_cloudfront_distribution.cloudfront.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
 resource "aws_route53_record" "cloudfront_aaaa" {
-  count   = "${(var.cloudfront_fqdn != "") ? 1 : 0}"
-  zone_id = "${data.aws_route53_zone.zone.zone_id}"
-  name    = "${var.cloudfront_fqdn}"
+  count   = var.cloudfront_fqdn != "" ? 1 : 0
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = var.cloudfront_fqdn
   type    = "AAAA"
 
   alias {
-    name                   = "${aws_cloudfront_distribution.cloudfront.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.cloudfront.hosted_zone_id}"
+    name                   = aws_cloudfront_distribution.cloudfront.domain_name
+    zone_id                = aws_cloudfront_distribution.cloudfront.hosted_zone_id
     evaluate_target_health = false
   }
 }
 
 resource "aws_cloudfront_distribution" "cloudfront" {
   origin {
-    domain_name = "${(var.create_bucket) ? join("", aws_s3_bucket.bucket.*.bucket_domain_name) : local.bucket_domain_name}"
-    origin_id   = "${var.create_bucket ? join("", aws_s3_bucket.bucket.*.id) : local.bucket_id}"
-    origin_path = "${var.cloudfront_origin_path}"
+    domain_name = var.create_bucket ? join("", aws_s3_bucket.bucket.*.bucket_domain_name) : local.bucket_domain_name
+    origin_id   = var.create_bucket ? join("", aws_s3_bucket.bucket.*.id) : local.bucket_id
+    origin_path = var.cloudfront_origin_path
 
     s3_origin_config {
-      origin_access_identity = "${(var.cloudfront_origin_access_identity_path == "") ? join("", aws_cloudfront_origin_access_identity.identity.*.cloudfront_access_identity_path) : var.cloudfront_origin_access_identity_path}"
+      origin_access_identity = var.cloudfront_origin_access_identity_path == "" ? join(
+        "",
+        aws_cloudfront_origin_access_identity.identity.*.cloudfront_access_identity_path,
+      ) : var.cloudfront_origin_access_identity_path
     }
   }
 
-  enabled             = "${var.cloudfront_enabled}"
-  is_ipv6_enabled     = "${var.cloudfront_ipv6_enabled}"
-  comment             = "${var.cloudfront_comment}"
-  default_root_object = "${var.cloudfront_default_root_object}"
-  aliases             = ["${compact(distinct(concat(list(var.cloudfront_fqdn),var.cloudfront_aliases)))}"]
-  price_class         = "${var.cloudfront_price_class}"
-  web_acl_id          = "${var.create_waf_acl ? element(concat(aws_waf_web_acl.cloudfront.*.id, list("")), 0) : ""}"
+  enabled             = var.cloudfront_enabled
+  is_ipv6_enabled     = var.cloudfront_ipv6_enabled
+  comment             = var.cloudfront_comment
+  default_root_object = var.cloudfront_default_root_object
+  aliases = compact(
+    distinct(concat([var.cloudfront_fqdn], var.cloudfront_aliases)),
+  )
+  price_class = var.cloudfront_price_class
+  web_acl_id  = var.create_waf_acl ? element(concat(aws_waf_web_acl.cloudfront.*.id, [""]), 0) : ""
 
   default_cache_behavior {
-    allowed_methods        = "${var.cloudfront_default_cache_allowed_methods}"
-    cached_methods         = "${var.cloudfront_default_cache_cached_methods}"
-    target_origin_id       = "${var.create_bucket ? join("", aws_s3_bucket.bucket.*.id) : local.bucket_id}"
-    compress               = "${var.cloudfront_default_cache_compress}"
-    default_ttl            = "${var.cloudfront_default_cache_default_ttl}"
-    max_ttl                = "${var.cloudfront_default_cache_max_ttl}"
-    min_ttl                = "${var.cloudfront_default_cache_min_ttl}"
-    viewer_protocol_policy = "${var.cloudfront_default_cache_viewer_protocol_policy}"
+    allowed_methods        = var.cloudfront_default_cache_allowed_methods
+    cached_methods         = var.cloudfront_default_cache_cached_methods
+    target_origin_id       = var.create_bucket ? join("", aws_s3_bucket.bucket.*.id) : local.bucket_id
+    compress               = var.cloudfront_default_cache_compress
+    default_ttl            = var.cloudfront_default_cache_default_ttl
+    max_ttl                = var.cloudfront_default_cache_max_ttl
+    min_ttl                = var.cloudfront_default_cache_min_ttl
+    viewer_protocol_policy = var.cloudfront_default_cache_viewer_protocol_policy
 
     forwarded_values {
-      query_string = "${var.cloudfront_default_cache_forwarded_values_query_string}"
-      headers      = ["${var.cloudfront_default_cache_forwarded_values_headers}"]
+      query_string = var.cloudfront_default_cache_forwarded_values_query_string
+      headers      = var.cloudfront_default_cache_forwarded_values_headers
 
       cookies {
-        forward = "${var.cloudfront_default_cache_forwarded_cookies}"
+        forward = var.cloudfront_default_cache_forwarded_cookies
       }
     }
   }
 
   restrictions {
     geo_restriction {
-      restriction_type = "${var.cloudfront_geo_restriction_type}"
+      restriction_type = var.cloudfront_geo_restriction_type
     }
   }
 
   viewer_certificate {
-    acm_certificate_arn      = "${data.aws_acm_certificate.cloudfront.arn}"
-    minimum_protocol_version = "${var.cloudfront_cert_min_supported_version}"
-    ssl_support_method       = "${var.cloudfront_cert_ssl_support_method}"
+    acm_certificate_arn      = data.aws_acm_certificate.cloudfront.arn
+    minimum_protocol_version = var.cloudfront_cert_min_supported_version
+    ssl_support_method       = var.cloudfront_cert_ssl_support_method
   }
 }
 
 resource "aws_waf_web_acl" "cloudfront" {
-  count       = "${var.create_waf_acl ? 1 : 0}"
-  name        = "acl_${var.cloudfront_fqdn}"
+  count = var.create_waf_acl ? 1 : 0
+  name  = "acl_${var.cloudfront_fqdn}"
+
   # CloudWatch metric names have strict restrictions on acceptable characters
-  metric_name = "${replace("acl${var.cloudfront_fqdn}", "/[^[:alnum:]]/", "")}"
+  metric_name = replace("acl${var.cloudfront_fqdn}", "/[^[:alnum:]]/", "")
 
   default_action {
-    type = "${var.waf_acl_default_action}"
+    type = var.waf_acl_default_action
   }
 }
 
 # outputs from edge tier
 
 output "cloudfront_domain_name" {
-  value = "${aws_cloudfront_distribution.cloudfront.domain_name}"
+  value = aws_cloudfront_distribution.cloudfront.domain_name
 }
 
 output "cloudfront_id" {
-  value = "${aws_cloudfront_distribution.cloudfront.id}"
+  value = aws_cloudfront_distribution.cloudfront.id
 }
 
 output "cloudfront_waf_acl_id" {
-  value = "${element(concat(aws_waf_web_acl.cloudfront.*.id, list("")), 0)}"
+  value = element(concat(aws_waf_web_acl.cloudfront.*.id, [""]), 0)
 }
+
